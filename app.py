bcd65e12dcf7d456800ebd4f22f8c0a04115fdaa
Woo Jaehyun <wjaehyun97@gmail.com>
2025-12-10 20:02:14 +0900

    streamlit app.py done

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 
+# 새로 불러와졌는지 terminal에서 체크
+print("page reloaded")
 st.set_page_config(
     page_title="포켓몬 도감",
     page_icon="./images/monsterball.png"
 )
+# custom css 반영을 위한 markdown
+st.markdown("""
+<style>
+img {
+    max_height: 300px;
+}
+</style>
+""", unsafe_allow_html=True)
 
 st.title("streamlit 포켓몬 도감")
 st.markdown("**포켓몬**을 하나씩 추가해서 도감을 채워보세요!")
@@ -29,55 +39,81 @@ type_emoji_dict = {
     "페어리": "🧚"
 }
 
-pokemons = [
+initial_pokemons = [
      {
     "name": "피카츄",
     "types": ["전기"],
-    "image_url": "https://i.namu.wiki/i/SVMK4eZf-H5dqybpTkReUG_iVr3Q4HXCPAd0lNPldimJlp41N_pCilPiR6QPghJgck28w_GgXdMAMrmnxo8-sUpYXAo1UDzu-daukUlLu22_BK9_sPRAmpgDWKE8QtCpMYFqyPpx1SWRtZlEk11Nfw.webp"
+    "image_url": 'images/피카츄.webp'
     },
     {
     "name": "누오",
     "types": ["물", "땅"],
-    "image_url": "https://i.namu.wiki/i/cDs6f1XZ8NqV0ieTNWrvDYxlOAqbAyM6cFwwMzdfckacs80Q7e0OrX7UYqlGOe8a-okHmHyFSRDJxJXcUgEjHTeTOAIfwazRyVdFYse5SgMl1Lrz66i2Ykkk4VFFbvvhTr-3tjzZg5TfxyML4iRCzQ.webp"
+    "image_url": "images/누오.webp"
     },
     {
     "name": "갸라도스",
     "types": ["물", "비행"],
-    "image_url": "https://i.namu.wiki/i/h9WMq1hyKoT5aR1WBmDCQSyGMKk-0UWSFw8GwDXYJyf1SGP7j6eA9txN_qZyzau6rOU4cW49X7Uv13wa6etYydkgvYJfH14KfW8UBgnB6DZUvgxJJzmVQ40RXVf63Lz7oLfSpSinJZkVWPo-K-OWXQ.webp"
+    "image_url": "images/갸라도스.webp"
     },
         {
     "name": "개굴닌자",
     "types": ["물"],
-    "image_url": "https://i.namu.wiki/i/2F-rWTt3FgKRh1h3SXItaa3xJWqtvdL714IKJ9DUnvtODZ4Y4mG0Ih4eULl_srIfL9_iLrtr1uFH9wYJpeDyStOlWrjhP1e936ctm_E6HCnn4KdARkXoTlhWRXAs6EhXg0o3CEONCYiQIU3HOVuRoA.webp"
+    "image_url": "images/개굴닌자.webp"
     },
     {
     "name": "루카리오",
     "types": ["격투", "강철"],
-    "image_url": "https://i.namu.wiki/i/_-_1FO_ztYci7Ji1iWJlmmJ15OufdWfEs91pm_fdSMnTUTOIIYFtAaTyKoxv0bXVmLwEXwM2uS_em0EvoPT7ZtLN5QmZvr6VzeS3vGPzDZzEqnimVFM7l671VVysfBsC4YE-P-cwU_xoyVcbnsbCiQ.webp"
+    "image_url": "images/루카리오.webp"
     },  
     {
     "name": "에이스번",
     "types": ["불꽃"],
-    "image_url": "https://i.namu.wiki/i/p7HMCkYJDeuETRy-Hevp7Hq5DygTQBwaLbloLFnGl8726Xti-TNP-3lk8g34bvqL-4b0t2ya57scGmPoWQYQ4VW4EdZVInzX17vdv3YbYG3-Od4bE3ZlT6wmlm4vBUHix-Vfmx5nMLgPsvbjN6F0VA.webp"
+    "image_url": "images/에이스번.webp"
     }
 ]
 
+example_pokemon = {
+     "name": "알로라 디그다",
+     "types": ["땅", "강철"],
+     "image_url": "images/알로라_디그다.png"
+ }
+
+# streamlit은 추가 업로드마다 새롭게 파일을 읽기 때문에
+# 세션을 남겨야 추가된 포켓몬이 덮어쓰기 되지 않는다.
+# 페이지를 지우거나 새로고침 할 때까지 남아있다.
+if 'pokemons' not in st.session_state:
+    st.session_state.pokemons = initial_pokemons
+
+
+# toggle을 이용한 form 자동완성
+auto_complete = st.toggle("예시 데이터로 채우기")
+print("page_reload, auto_complete", auto_complete)
 # 포켓몬 추가 버튼
 with st.form(key="form"):
     col1, col2 = st.columns(2)
     # 이름 입력
     with col1:
-        name = st.text_input(label="포켓몬 이름")
+        name = st.text_input(
+            label="포켓몬 이름",
+            # toggle 폼 자동 완성
+            value=example_pokemon["name"] if auto_complete else ""
+        )
     # 타입 선택
     with col2:
         types = st.multiselect(
             label="포켓몬 타입",
             options=list(type_emoji_dict.keys()),
             # 타입은 2개까지만
-            max_selections=2
+            max_selections=2,
+            # toggle 폼 자동 완성
+            default=example_pokemon["types"] if auto_complete else []
         )
     # 이미지 추가하기
-    image_url = st.text_input(label="포켓몬 이미지 URL")
+    image_url = st.text_input(
+        label="포켓몬 이미지 URL",
+        # toggle 폼 자동 완성
+        value=example_pokemon["image_url"] if auto_complete else ""
+    )
     # 업로드
     submit = st.form_submit_button(label="Submit")
     if submit:
@@ -88,7 +124,7 @@ with st.form(key="form"):
             st.error("포켓먼의 타입을 적어도 한 개 선택해주세요.")
         else:
             st.success("포켓몬을 추가할 수 있습니다.")
-            pokemons.append({
+            st.session_state.pokemons.append({
                 "name": name,
                 "types": types,
                 # 이미지가 있을 때는 이미지, 없으면 기본이미지
@@ -97,9 +133,9 @@ with st.form(key="form"):
 
 
 # 포켓몬을 이름/이미지/타입 출력
-for i in range(0, len(pokemons), 3):
+for i in range(0, len(st.session_state.pokemons), 3):
     # 3개씩 번호대로 출력
-    row_pokemons = pokemons[i:i+3]
+    row_pokemons = st.session_state.pokemons[i:i+3]
     cols = st.columns(3)
     for j in range(len(row_pokemons)):
         with cols[j]:
@@ -107,4 +143,11 @@ for i in range(0, len(pokemons), 3):
             with st.expander(label=f"**{i+1+j}. {pokemon['name']}**", expanded = True):
                 st.image(pokemon["image_url"])
                 emoji_types = [f"{type_emoji_dict[x]} {x}" for x in pokemon["types"]]
-                st.text(" / ".join(emoji_types))        
+                st.text(" / ".join(emoji_types))
+                # 삭제 버튼을 박스 크기에 맞게 제작/조절
+                delete_button = st.button(label="삭제", key=i+j, use_container_width=True)
+                if delete_button:
+                    print("delete button clicked!")
+                    del st.session_state.pokemons[i+j]
+                    # rerun()으로 reload동작을 바로 반영/원하는 포켓몬 삭제 가능
+                    st.rerun()
